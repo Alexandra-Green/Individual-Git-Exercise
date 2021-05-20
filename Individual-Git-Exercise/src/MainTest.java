6a4b5a22ae07636d043dd36d654800734736db70
Alexandra-Green <60950452+Greeny@users.noreply.github.com>
2021-05-19 18:37:17 -0700

    added inline comment

--- Individual-Git-Exercise/src/MainTest.java
+++ Individual-Git-Exercise/src/MainTest.java
@@ -11,6 +11,7 @@ public class MainTest {
 		//expected output is 4
 		System.out.println(output2);
 		int output3 = trial.count("1234561125214163611");
+		//expected output is 3
 		System.out.println(output3);
 		int output4 = trial.count("11   5  53  57   6  43  46  23  12   4");
 		System.out.println(output4);
